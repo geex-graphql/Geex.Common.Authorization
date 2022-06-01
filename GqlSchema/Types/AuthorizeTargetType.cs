07633803d82c001f1244490ddc94e42060eeeb70
Lulus <snys98@outlook.com>
2022-06-01 09:45:04 +0800

    update

--- GqlSchema/Types/AuthorizeTargetType.cs
+++ GqlSchema/Types/AuthorizeTargetType.cs
@@ -2,7 +2,7 @@
 
 namespace Geex.Common.Authorization.GqlSchema.Types
 {
-    public class AuthorizeTargetType : Enumeration<AuthorizeTargetType, string>
+    public class AuthorizeTargetType : Enumeration<AuthorizeTargetType>
     {
         public static AuthorizeTargetType Role { get; } = new AuthorizeTargetType(nameof(Role));
         public static AuthorizeTargetType User { get; } = new AuthorizeTargetType(nameof(User));
